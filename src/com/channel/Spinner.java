2afe5271fe8d5c798baaed6a5000af47c1f13d6e
shashank-a <shashank.ashokkumar@a-cti.com>
2014-06-19 15:16:50 +0530

    Channel Server Configuration

---
+++ src/com/channel/Spinner.java
@@ -0,0 +1,26 @@
+package com.channel;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class Spinner extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+//		System.out.println(req.getMethod()+":Pinging Check = "+new Date());
+		resp.setContentType("text/plain");
+		resp.getWriter().println(req.getMethod()+":Spinning Instance");
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+//		System.out.println(req.getMethod()+":Pinging Check = "+new Date());
+		resp.setContentType("text/plain");
+		resp.getWriter().println(req.getMethod()+":Spinning Instance");
+	}
+}
